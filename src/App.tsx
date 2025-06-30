cd63a5d9b87a76a307b72063eb010fa855e8bed1
bpinela <bruno.pinelamarques@gmail.com>
2025-06-29 22:35:34 -0300

    feat: setup eslint, prettier, husky and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline bg-amber-500">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
