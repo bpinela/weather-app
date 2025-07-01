15c7359fa53fb3ad06f53f213c3f6219548298f6
bpinela <bruno.pinelamarques@gmail.com>
2025-07-01 17:04:13 -0300

    feat: create root page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-'use client'
-
-import { useWeatherData } from './hooks/useWeatherData'
+import { useRef } from 'react'
+import type React from 'react'
+import { useWeatherData } from '@/hooks/useWeatherData'
+import { WeatherContent } from '@/components/WeatherContent'
+import { Sidebar } from '@/components/Sidebar'
 
 export default function App() {
-  const { weatherData } = useWeatherData('Chicago')
+  const searchInputRef = useRef<HTMLInputElement>(null)
+  const { weatherData, loading, error, searchWeather } = useWeatherData('Miami')
 
-  console.log(weatherData)
+  const handleKeyPress = async (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      const city = searchInputRef.current?.value.trim()
+      if (city) {
+        await searchWeather(city)
+        if (searchInputRef.current) {
+          searchInputRef.current.value = ''
+        }
+      }
+    }
+  }
 
-  return <div className="flex h-screen w-screen"></div>
+  return (
+    <div className="flex h-screen w-screen">
+      <Sidebar
+        searchInputRef={searchInputRef}
+        handleKeyPress={handleKeyPress}
+        loading={loading}
+        error={error}
+      />
+
+      <WeatherContent weatherData={weatherData} loading={loading} />
+    </div>
+  )
 }
